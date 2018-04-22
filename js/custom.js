513dc75efaf0442d0416f4b057bfd0f19190cb63
Thomas Eizinger <thomas@eizinger.io>
2018-04-22 11:09:31 +1000

    Remove php stuff and everything related to the form

--- js/custom.js
+++ js/custom.js
@@ -144,50 +144,7 @@
 				$(PlaceV).html('<iframe src="' + videoLink + '?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1&amp;color=7b5eea" width="500" height="281" frameborder="0"></iframe>');
 			}
 		});	
-		
-		
-		/* Contact form */		
 
-		$('#contact-form').validator();
-
-
-		// when the form is submitted
-		$('#contact-form').on('submit', function (e) {
-
-			// if the validator does not prevent form submit
-			if (!e.isDefaultPrevented()) {
-				var url = "contact.php";
-
-				// POST values in the background the the script URL
-				$.ajax({
-					type: "POST",
-					url: url,
-					data: $(this).serialize(),
-					success: function (data)
-					{
-						// data = JSON object that contact.php returns
-
-						// we recieve the type of the message: success x danger and apply it to the 
-						var messageAlert = 'alert-' + data.type;
-						var messageText = data.message;
-
-						// let's compose Bootstrap alert box HTML
-						var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-						
-						// If we have messageAlert and messageText
-						if (messageAlert && messageText) {
-							// inject the alert to .messages div in our form
-							$('#contact-form').find('.messages').html(alertBox);
-							// empty the form
-							$('#contact-form')[0].reset();
-						}
-					}
-				});
-				return false;
-			}
-		})
-
-			
 						
 	});
 
